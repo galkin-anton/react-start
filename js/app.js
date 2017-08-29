e4fe1f91cce23fdb1ee56bc79d2f6cc4f47ba3ea
Galkin_AB <galkin88@gmail.com>
2017-08-29 17:42:19 +0500

    add some messages

--- js/app.js
+++ js/app.js
@@ -1,11 +1,70 @@
+var myNews = [
+  {
+    author: 'Galkin_AB',
+    text: 'News 1'
+  },
+  {
+    author: 'Galkin_AB',
+    text: 'News 2'
+  },
+  {
+    author: 'Galkin_AB',
+    text: 'News 3'
+  },
+  {
+    author: 'Galkin_AB',
+    text: 'News 3'
+  }
+];
+
 var News = React.createClass({
   render: function() {
-    return <h1>Hello React {this.props.name}</h1>;
+    let data = this.props.data;
+    let template;
+    if (data.length > 0) {
+      template = data.map(function(news, index) {
+        return (
+          <div key={index}>
+            <p className="news__author">
+              {news.author}:
+            </p>
+            <p className="news__text">
+              {news.text}
+            </p>
+          </div>
+        );
+      });
+    } else {
+      template = <div>Сообщений нет</div>;
+    }
+
+    return (
+      <div>
+        {template}
+        <strong className={data.length > 0 ? '' : 'none'}>
+          Всего новостей {data.length}
+        </strong>
+      </div>
+    );
   }
 });
+
+var Comments = React.createClass({
+  render: function() {
+    return <div className="comments">Комментариев нет</div>;
+  }
+});
+
 var App = React.createClass({
   render: function() {
-    return <News name="hhh" />;
+    return (
+      <div className="app">
+        HI! I am react component!
+        <News data={myNews} />
+        <Comments />
+      </div>
+    );
   }
 });
+
 ReactDOM.render(<App />, document.getElementById('root'));
