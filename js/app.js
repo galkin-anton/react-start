a9829e5dc85c41ab60a640f8d9ac4c938b9b037e
anton <galkin88@gmail.com>
2017-08-30 22:31:06 +0500

    add click action on a tag

--- js/app.js
+++ js/app.js
@@ -1,42 +1,75 @@
 var myNews = [
   {
     author: 'Galkin_AB',
-    text: 'News 1'
+    text: 'News 1',
+    bigText: 'big text1'
   },
   {
     author: 'Galkin_AB',
-    text: 'News 2'
+    text: 'News 2',
+    bigText: 'big text2'
   },
   {
     author: 'Galkin_AB',
-    text: 'News 3'
+    text: 'News 3',
+    bigText: 'big text3'
   },
   {
     author: 'Galkin_AB',
-    text: 'News 3'
+    text: 'News 4',
+    bigText: 'big text4'
   }
 ];
 // myNews = [];
 
 class Article extends React.Component {
-  render() {
-    let author = this.props.data.author;
-    let text = this.props.data.text;
+  constructor(props) {
+    super(props);
+    this.state = { visible: false };
+    this.click = this.click.bind(this);
+  }
 
+  click(e) {
+    e.preventDefault();
+    this.setState(prev => {
+      return { visible: !prev.visible };
+    });
+
+    console.log(this.props);
+    console.log('Clicked with scope', this);
+  }
+
+  render() {
+    let { author, text, bigText } = this.props.data;
     return (
       <div className="article">
-        <p className="news__author">
-          {author}:
-        </p>
-        <p className="news__text">
-          {text}
+        <p className="news__author">{author}:</p>
+        <p className="news__text">{text}</p>
+        <a href="#" className="news__readmore" onClick={this.click}>
+          {' '}
+          Подробнее{' '}
+        </a>
+        <p className="news__big-text">{bigText}</p>
+        <p className={this.state.visible ? '' : 'none'}>
+          Это доплнительная информация
         </p>
       </div>
     );
   }
 }
 
+Article.propTypes = {
+  data: React.PropTypes.shape({
+    author: React.PropTypes.string.isRequired,
+    text: React.PropTypes.string.isRequired
+  })
+};
+
 class News extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     let data = this.props.data;
     let template;
@@ -63,6 +96,10 @@ class News extends React.Component {
   }
 }
 
+News.propTypes = {
+  data: React.PropTypes.array.isRequired
+};
+
 class Comments extends React.Component {
   render() {
     return <div className="comments">Комментариев нет</div>;
@@ -75,6 +112,7 @@ class App extends React.Component {
       <div className="app">
         <h3>Новости</h3>
         <News data={myNews} />
+        {/*data={myNews}*/}
         <Comments />
       </div>
     );
